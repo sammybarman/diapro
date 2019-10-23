c50e46d8dd9c56b326ed549086d7a4f6c193fc8c
Sammy <sammy@Sammys-MacBook-Pro.local>
2019-10-23 16:49:13 +0530

    First Commit

--- static/js/init.js
+++ static/js/init.js
@@ -1,28 +1,53 @@
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-  });
-  var autoplay = true;
-  setInterval(function() { if(autoplay) $('.carousel.carousel-slider').carousel('next'); }, 5000);
-  $('.carousel.carousel-slider').hover(function(){ autoplay = false; },function(){ autoplay = true; });
-
 $(document).ready(function(){
-    $('.parallax').parallax();
-  });
+  $('.sidenav').sidenav();
+  $('.parallax').parallax();
+});
 
-$(document).ready(function(){
-      $('.fixed-action-btn').floatingActionButton();
+$(window).scroll(function(){
 
-  });
+  if($(window).scrollTop()>20){
+    $('nav').addClass('bg');
+    $('.brand-logo').addClass('black-text');
+    $('.forjq').addClass('bg2');
+  }else{
+    $('nav').removeClass('bg');
+    $('.brand-logo').removeClass('black-text');
+    $('.forjq').removeClass('bg2');
+  }
+});
 
-$(document).ready(function(){
 
-      $(window).scroll(function(){
 
-       if($(window).scrollTop()>20){
-         $('nav').addClass('bg');
-       }else{
-         $('nav').removeClass('bg');
-       }
-    });
-  });
+
+
+
+
+
+// $('.carousel.carousel-slider').carousel({
+//     fullWidth: true,
+//     indicators: true
+//   });
+//   var autoplay = true;
+//   setInterval(function() { if(autoplay) $('.carousel.carousel-slider').carousel('next'); }, 5000);
+//   $('.carousel.carousel-slider').hover(function(){ autoplay = false; },function(){ autoplay = true; });
+//
+// $(document).ready(function(){
+//     $('.parallax').parallax();
+//   });
+//
+// $(document).ready(function(){
+//       $('.fixed-action-btn').floatingActionButton();
+//
+//   });
+//
+// $(document).ready(function(){
+//
+//       $(window).scroll(function(){
+//
+//        if($(window).scrollTop()>20){
+//          $('nav').addClass('bg');
+//        }else{
+//          $('nav').removeClass('bg');
+//        }
+//     });
+//   });
